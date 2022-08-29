789b137078b925298c3ba8a6a3c65f74a77d0d8e
Ian Larsen <ianmonty007@gmail.com>
2022-08-29 17:25:52 -0600

    Variables test assignment completed

---
+++ variableTest/variableTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace variableTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int apples = 1;
+            int oranges = 2;
+            float camelCasedRapscallion = 3.3f;
+            double standardDeviationFromTheMean = 2.33334;
+            bool thisIsTrue = true;
+            bool thisIsFalse = false;
+            string welcomeMessage = "Hey howdy hi";
+            string unwelcomeMessage = "Go away >:(";
+            char firstOfAlphabet = 'a';
+            char secondOfAlphabet = 'b';
+
+            Console.WriteLine(apples);
+            Console.WriteLine(oranges);
+            Console.WriteLine(camelCasedRapscallion);
+            Console.WriteLine(standardDeviationFromTheMean);
+            Console.WriteLine(thisIsTrue);
+            Console.WriteLine(thisIsFalse);
+            Console.WriteLine(welcomeMessage);
+            Console.WriteLine(unwelcomeMessage);
+            Console.WriteLine(firstOfAlphabet);
+            Console.WriteLine(secondOfAlphabet);
+        }
+    }
+}
